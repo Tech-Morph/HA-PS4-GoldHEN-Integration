4fd74afc930289a5dec9821abe4316a7b6037152
Tech-Morph <205584457+Tech-Morph@users.noreply.github.com>
2026-02-18 19:57:53 -0800

    Add const.py with configuration constants

---
+++ custom_components/ps4_goldhen/const.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+DOMAIN = "ps4_goldhen"
+
+DEFAULT_BINLOADER_PORT = 9090
+DEFAULT_FTP_PORT = 2121
+
+CONF_HOST = "host"
+CONF_BINLOADER_PORT = "binloader_port"
+CONF_FTP_PORT = "ftp_port"
+
+PLATFORMS: list[str] = ["sensor"]
