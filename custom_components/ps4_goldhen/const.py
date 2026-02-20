570fa3e1adb12f07f6cbb5ca06304b649dffae44
Tech-Morph <205584457+Tech-Morph@users.noreply.github.com>
2026-02-19 16:01:02 -0800

    fix(const): add CONF_PS4_HOST and DEFAULT_PS4_HOST back

--- custom_components/ps4_goldhen/const.py
+++ custom_components/ps4_goldhen/const.py
@@ -4,9 +4,11 @@ DOMAIN = "ps4_goldhen"
 
 # Config entry keys
 CONF_ADDON_URL = "addon_url"  # e.g. http://192.168.1.50:8787
+CONF_PS4_HOST = "ps4_host"   # e.g. 192.168.1.14
 
 # Defaults
 DEFAULT_ADDON_URL = "http://192.168.1.50:8787"
+DEFAULT_PS4_HOST = "192.168.1.14"
 
 # Add-on API endpoints
 ENDPOINT_STATUS = "/status"
